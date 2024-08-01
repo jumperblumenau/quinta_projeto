13ab00173628ee4dd8a884d70f33513f141b1f67
almb2564 <almb2564@gmail.com>
2024-08-01 20:54:56 -0300

    alex

--- jogo_forca/jogoforca.py
+++ jogo_forca/jogoforca.py
@@ -1,14 +1,83 @@
 ##CRIAR NESTA PAGINA UM JOGO DE FORCA##
 
-def forca():
-    print("  _______     ")
-    print(" |/      |    ")
+def jogo_da_forca():
+    print('----x----x---- JOGO DA FORCA ----x----x----')
+    print('----- ADIVINHE O NOME DE UMA CIDADE -----\n')
 
-    print(" |            ")
 
-    print(" |            ")
+import random as rd
 
-    print(" |            ")
+lista = ['BLUMENAU', 'CURITIBA', 'ARARANGUÁ', 'JOINVILLE', 'BELO HORIZONTE', 'CAMPINAS', 'RIO DE JANEIRO', 'FLORIANOPOLIS', 'MANGUEIRINHA', '']
 
-    print("_|___         ")
-aldjkf
+
+# Função para escolher uma palavra da lista aleatoriamente
+def palavra_aleatoria(lista):
+    tamanho = len(lista)
+    sorteio = rd.randint(1, tamanho)
+    palavra_aleatoria = lista[sorteio]
+    return palavra_aleatoria
+
+
+# função para localizar as posições de uma letra em uma palavra
+def localizar(texto, palavra):
+    posicoes = []
+    for i in range(0, len(texto)):
+        if texto[i] == palavra:
+            posicoes.append(i)
+    return posicoes
+
+
+palavra = palavra_aleatoria(lista)
+print(f'DICA: A palavra tem {len(palavra)} letras')
+chances = 0
+pal_secreta = list('_' * len(palavra))
+while chances <= 10:
+    letra_digitada = input('Digite somente uma letra: ').upper()
+    if letra_digitada in palavra:
+        print(f'A letra {letra_digitada} está contida na frase')
+        posicao = localizar(palavra, letra_digitada)
+        for i in posicao:
+            pal_secreta[i] = letra_digitada
+        print(pal_secreta)
+        opcao = input('Você já sabe qual é o nome da palavra? (S ou N): ')
+        if opcao.upper() == 'S':
+            resposta = input('Digite o nome da palavra: ')
+            if resposta.upper() == palavra:
+                print('-------- X -------- X --------')
+                print(f'Parabéns!!\nA Palavra correta é {palavra}! ')
+                print('-------- X -------- X --------')
+                break
+            else:
+                print('Você errou!')
+                print(f'A Palavra correta é {palavra}!')
+                print('-------- X GAME OVER X --------')
+                break
+        else:
+            print('-------- X -------- X --------\n')
+            print(f'Você ainda tem mais {9 - int(chances)} chances')
+    else:
+        print(f'A letra {letra_digitada} NÃO está contida na frase')
+        print(pal_secreta)
+        opcao = input('Você já sabe qual é o nome da palavra? (S ou N): ')
+        if opcao.upper() == 'S':
+            resposta = input('Digite o nome da palavra: ')
+            if resposta.upper() == palavra:
+                print('-------- X -------- X --------')
+                print(f'Parabéns!!\nA Palavra correta é {palavra}!')
+                print('-------- X -------- X --------')
+                break
+            else:
+                print('Você errou!')
+                print(f'A Palavra correta é {palavra}!')
+                print('-------- X GAME OVER X --------')
+                break
+        else:
+            print(f'Você ainda tem mais {9 - int(chances)} chances')
+            print('-------- X -------- X --------\n')
+
+    chances += 1
+    if chances >= 7:
+        print('-------- X GAME OVER X --------')
+        print(f'A Palavra correta é {palavra}!')
+        break
+
