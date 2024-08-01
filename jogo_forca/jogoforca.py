107dc7bcefe4b55f7c00f7932f24bc5e0d4b9ea3
jumperblumenau <jumperblumenau2018@gmail.com>
2024-08-01 20:53:51 -0300

    matheus

--- jogo_forca/jogoforca.py
+++ jogo_forca/jogoforca.py
@@ -1,15 +1,130 @@
  ##CRIAR NESTA PAGINA UM JOGO DE FORCA##
 
-def forca():
-    print("  _______     ")
-    print(" |/      |    ")
+import random
 
-    print(" |            ")
+# Lista de palavras aleatórias
+palavras = [
+    "python", "programacao", "computador", "jogo", "forca",
+    "desenvolvimento", "dados", "algoritmo", "tecnologia", "internet"
+]
 
-    print(" |            ")
 
-    print(" |            ")
+def escolher_palavra():
+    return random.choice(palavras)
 
-    print("_|___         ")
 
-vdv
+def exibir_forca(erros):
+    estados = [
+        """
+           -----
+           |   |
+               |
+               |
+               |
+               |
+        --------
+        """,
+        """
+           -----
+           |   |
+           O   |
+               |
+               |
+               |
+        --------
+        """,
+        """
+           -----
+           |   |
+           O   |
+           |   |
+               |
+               |
+        --------
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|   |
+               |
+               |
+        --------
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|\\  |
+               |
+               |
+        --------
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|\\  |
+          /    |
+               |
+        --------
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|\\  |
+          / \\  |
+               |
+        --------
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|\\  |
+          / \\  |
+               |
+        --------
+        """
+    ]
+    print(estados[erros])
+
+
+def jogar():
+    palavra = escolher_palavra()
+    letras_adivinhadas = []
+    tentativas = 10
+    letras_erradas = []
+
+    print("Bem-vindo ao Jogo da Forca!")
+
+    while tentativas > 0:
+        palavra_oculta = "".join([letra if letra in letras_adivinhadas else "_" for letra in palavra])
+
+        print("\nPalavra: ", palavra_oculta)
+        print("Letras erradas: ", " ".join(letras_erradas))
+        print(f"Tentativas restantes: {tentativas}")
+
+        letra = input("Digite uma letra: ").lower()
+
+        if letra in letras_adivinhadas or letra in letras_erradas:
+            print("Você já tentou essa letra. Tente outra.")
+            continue
+
+        if letra in palavra:
+            letras_adivinhadas.append(letra)
+            if all(letra in letras_adivinhadas for letra in palavra):
+                print(f"Parabéns! Você adivinhou a palavra: {palavra}")
+                break
+        else:
+            letras_erradas.append(letra)
+            tentativas -= 1
+            exibir_forca(len(letras_erradas))
+
+        if tentativas == 0:
+            print(f"Você perdeu! A palavra era: {palavra}")
+
+
+if __name__ == "__main__":
+    jogar()
