0784a0f49facd4bb4fcc1c8b706d080490090bec
jumperblumenau <jumperblumenau2018@gmail.com>
2024-08-01 19:02:34 -0300

    #TASK1

--- jogo_forca/jogoforca.py
+++ jogo_forca/jogoforca.py
@@ -1,3 +1,13 @@
 ##CRIAR NESTA PAGINA UM JOGO DE FORCA##
 
+def forca():
+    print("  _______     ")
+    print(" |/      |    ")
 
+    print(" |            ")
+
+    print(" |            ")
+
+    print(" |            ")
+
+    print("_|___         ")
